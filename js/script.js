c7ece2910025dd174e07643bf4489f20af6ea44f
Kyryl <sanchenko57@gmail.com>
2022-05-24 12:35:15 +0300

    firtt commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = +prompt("Are u 18 yeards old?", "18");
+// console.log(answer + 5);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Your name?", '');
+// answers[1] = prompt("Your surname?", '');
+// answers[2] = +prompt("Your age?", '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const category = "toys";
+// console.log(`https://wpset.net/${category}/5`);
+
+// const user = "Kyryl";
+// alert(`Hello, ${user}`);
+
+// console.log(`\``);
+
+// console.log('string text line 1\n' +
+// 'string text line 2');
+// console.log(`string text line 1
+// string text line 2`);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(10%3);
